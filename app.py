7a1aa0b5f98f119dfa2d65ec215781c262af332d
Gayathri <g3saga5335@gmail.com>
2025-01-16 21:32:43 +0530

    Initial commit

--- app.py
+++ app.py
@@ -25,32 +25,40 @@ def frontend():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get form inputs
-    print(request.form)  
-    temperature = float(request.form['temperature'])
-    humidity = float(request.form['humidity'])
-    pm25 = float(request.form['pm25'])
-    pm10 = float(request.form['pm10'])
-    so2 = float(request.form['so2'])
-    no2 = float(request.form['no2'])
-    co = float(request.form['co'])
-    proximity = float(request.form['proximity'])
-    population = float(request.form['population'])
-    
-    pm = pm25 - pm10
-    # Create feature array
-    features = pd.DataFrame([[temperature, humidity, pm, no2, so2, co, proximity, population]], 
-                        columns=['Temperature', 'Humidity', 'PM', 'NO2', 'SO2', 'CO', 'Proximity_to_Industrial_Areas', 'Population_Density'])
+    if request.method == 'POST':
+            print(request.form)  
+            temperature = float(request.form['temperature'])
+            humidity = float(request.form['humidity'])
+            pm25 = float(request.form['pm25'])
+            pm10 = float(request.form['pm10'])
+            so2 = float(request.form['so2'])
+            no2 = float(request.form['no2'])
+            co = float(request.form['co'])
+            proximity = float(request.form['proximity'])
+            population = float(request.form['population'])
+            
+            pm = pm25 - pm10
+            # Create feature array
+            features = pd.DataFrame([[temperature, humidity, pm, no2, so2, co, proximity, population]], 
+                                columns=['Temperature', 'Humidity', 'PM', 'NO2', 'SO2', 'CO', 'Proximity_to_Industrial_Areas', 'Population_Density'])
 
-    
-    # Scale features
-    features_scaled = scaler.transform(features)
-    scaled_features_df = pd.DataFrame(features_scaled, columns=features.columns)
+            
+            # Scale features
+            features_scaled = scaler.transform(features)
+            scaled_features_df = pd.DataFrame(features_scaled, columns=features.columns)
 
-    # Predict using the model
-    prediction = model.predict(scaled_features_df)
+            # Predict using the model
+            prediction = model.predict(scaled_features_df)
+            prediction = int(prediction[0])
+            # Return result as a JSON response
+            
+            result_message = "Air Quality is Good" if (prediction == 1 )else "Air Quality is Poor"
+            print({'Prediction': result_message})
+            return jsonify({'Prediction': result_message})
     
-    # Return result as a JSON response
-    return jsonify({'prediction': int(prediction[0])})
+            #return render_template('frontend.html', result_message=result_message)
+
+    #return render_template('frontend.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
